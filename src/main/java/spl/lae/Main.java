04252d8a54b7bf7b9fd57b6723055719804567c4
zoxarc <zoxarc@gmail.com>
2025-12-17 14:45:17 +0200

    Initial commit

---
+++ src/main/java/spl/lae/Main.java
@@ -0,0 +1,11 @@
+package spl.lae;
+import java.io.IOException;
+
+import parser.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      // TODO: main
+	  System.out.println("hello world");
+    }
+}
