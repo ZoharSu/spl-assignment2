08e99a2a7e66725c5daf407b88c0bd01f0fbca95
zoxarc <zoxarc@gmail.com>
2025-12-27 08:28:52 +0200

    Finish Main

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -5,6 +5,23 @@ import parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      // TODO: main
+        if (args.length < 4) {
+            System.out.println("Usage: lae <number of threads> <input path> <output path>");
+            return;
+        }
+
+        int thread_num = Integer.parseInt(args[1]);
+        // String input = args[2], output = args[3];
+        InputParser input = new InputParser();
+        ComputationNode root;
+        try { root = input.parse(args[2]);
+        } catch (Exception e) {
+            // TODO: error message
+            return;
+        }
+
+        LinearAlgebraEngine engine = new LinearAlgebraEngine(thread_num);
+        engine.run(root);
+        OutputWriter.write(root.getMatrix(), args[3]);
     }
 }
